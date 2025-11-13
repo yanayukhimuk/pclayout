f852e10b0c317bf7045373304ecd5074ecdb1d14
y.glebik <yana.ryzhikova.brest.belarus@gmail.com>
2025-11-13 09:57:27 +0300

    Made base task 4

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,24 @@
-const modalBtn = document.querySelector('.modal__button')
-const orderBtn = document.querySelector('.course__button')
-const modal = document.querySelector('.modal')
-const closeModalBtn = document.querySelector('.modal__close')
+const modal = () => {
+    const modalBtn = document.querySelector('.modal__button')
+    const orderBtn = document.querySelector('.course__button')
+    const modal = document.querySelector('.modal')
+    const closeModalBtn = document.querySelector('.modal__close')
 
-modalBtn.addEventListener('click', () => {
-    modal.style.display = 'flex'
-})
-orderBtn.addEventListener('click', () => {
-    modal.style.display = 'flex'
-})
-closeModalBtn.addEventListener('click', () => {
-    modal.style.display = ''
-})
-modal.addEventListener('click', (event) => {
-    const modalContent = event.target.closest('.modal__inner')
-    if (!modalContent) {
+    modalBtn.addEventListener('click', () => {
+        modal.style.display = 'flex'
+    })
+    orderBtn.addEventListener('click', () => {
+        modal.style.display = 'flex'
+    })
+    closeModalBtn.addEventListener('click', () => {
         modal.style.display = ''
-    }
-})
+    })
+    modal.addEventListener('click', (event) => {
+        const modalContent = event.target.closest('.modal__inner')
+        if (!modalContent) {
+            modal.style.display = ''
+        }
+    })
+}
+
+modal()
--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,16 +1,20 @@
-const navbar = document.querySelector('.header__nav')
-const links = navbar.querySelectorAll('a')
+const smoothScroll = () => {
+    const navbar = document.querySelector('.header__nav')
+    const links = navbar.querySelectorAll('a')
 
-links.forEach((link) => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault()
-        const section = document.querySelector(link.getAttribute('href'))
-        if (section) {
-            seamless.scrollIntoView(section, {
-                behavior: "smooth",
-                block: "start",
-                inline: "center",
-            })
-        }
+    links.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault()
+            const section = document.querySelector(link.getAttribute('href'))
+            if (section) {
+                seamless.scrollIntoView(section, {
+                    behavior: "smooth",
+                    block: "start",
+                    inline: "center",
+                })
+            }
+        })
     })
-})
+}
+
+smoothScroll()
